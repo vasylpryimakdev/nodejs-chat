08e4ab5c39467d61eb5494a51b9c6ed85dac3faa
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-29 20:10:02 +0300

    feat: implement basic TCP chat with broadcast messaging

--- client.js
+++ client.js
@@ -1,9 +1,25 @@
 const net = require("net");
+const readline = require("readline/promises");
 
-const client = net.createConnection({ host: "127.0.0.1", port: 3008 }, () => {
-  console.log("Connected to the server!");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-client.on("end", () => {
+const socket = net.createConnection(
+  { host: "127.0.0.1", port: 3008 },
+  async () => {
+    console.log("Connected to the server!");
+
+    const message = await rl.question("Enter a message > ");
+    socket.write(message);
+  }
+);
+
+socket.on("data", (data) => {
+  console.log(data.toString("utf-8"));
+});
+
+socket.on("end", () => {
   console.log("Connection was ended!");
 });
--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const net = require("net");
 
 const server = net.createServer();
 
+// an array of client sockets
+const clients = [];
+
 server.on("connection", (socket) => {
   console.log("A new connection to the server!");
+
+  socket.on("data", (data) => {
+    clients.map((s) => {
+      s.write(data);
+    });
+  });
+
+  clients.push(socket);
 });
 
 server.listen(3008, "127.0.0.1", () => {
