54f42655474e0cc3f2aa06b50291593607fed379
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-30 12:12:55 +0300

    feat: add client identification and tagged messaging to chat server

--- server.js
+++ server.js
@@ -8,13 +8,20 @@ const clients = [];
 server.on("connection", (socket) => {
   console.log("A new connection to the server!");
 
+  const clientId = clients.length + 1;
+  socket.write(`id-${clientId}`);
+
   socket.on("data", (data) => {
-    clients.map((s) => {
-      s.write(data);
+    const dataString = data.toString("utf-8");
+    const id = dataString.substring(0, dataString.indexOf("-"));
+    const message = dataString.substring(dataString.indexOf("-message-") + 9);
+
+    clients.map((client) => {
+      client.socket.write(`> User ${id}: ${message}`);
     });
   });
 
-  clients.push(socket);
+  clients.push({ id: clientId.toString(), socket });
 });
 
 server.listen(3008, "127.0.0.1", () => {
